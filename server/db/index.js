87f18d535e77e09b5526ff2998b41271ab1095c4
rdripley <russellripley@hotmail.com>
2020-04-08 15:54:54 -0700

    added mongodb as dependency. Removed localhost connection and added Heroku syntax

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/HangmanGame", { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .catch(e => {
     console.error("Connection error", e.message);
   });
